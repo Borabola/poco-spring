f912c19daddec2a625a7c14bf69d7173ec4f0e9c
Olha Sokolova <osokolova1@gmail.com>
2024-12-06 15:26:50 +0100

    Spring app init

---
+++ src/main/java/com/example/pocospring/PocoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pocospring/PocoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
