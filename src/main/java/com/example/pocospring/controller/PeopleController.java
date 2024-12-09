515a165e8397082a77149e98f86bb6d66b9c010c
Olha Sokolova <osokolova1@gmail.com>
2024-12-09 15:16:03 +0100

    Use PersonService with @AllArgsConstructor

--- src/main/java/com/example/pocospring/controller/PeopleController.java
+++ src/main/java/com/example/pocospring/controller/PeopleController.java
@@ -2,6 +2,10 @@ package com.example.pocospring.controller;
 
 import com.example.pocospring.model.Person;
 import com.example.pocospring.service.PersonService;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +14,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+//@RequiredArgsConstructor // + @NonNull
+@AllArgsConstructor  //if not from lombok lib - need to add constructor
 public class PeopleController {
+    //@Autowired //old, @RequiredArgsConstructor is better
+    private final PersonService personService; //= new PersonService();   //temp, should be replaced by Spring logic
 
-    private PersonService personService = new PersonService();   //temp, should be replaced by Spring logic
-
+    //@NonNull
     @GetMapping
     public List<Person> findAllPeople() {
         //todo
